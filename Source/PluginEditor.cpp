2a1a48022586235586c4c9551a77da598aac6fdf
sursindmitry <dmitrijsursin@gmail.com>
2021-05-29 20:08:22 +0700

    Test

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -16,6 +16,7 @@ SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor (SimpleEQAudioProcess
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (400, 300);
+    
 }
 
 SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor()
